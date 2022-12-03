8a6e2a01160c91d2795e35b20b9a087a1a0e2544
Wolfgang Maurer <wma@aneo.at>
2022-12-03 22:17:09 +0100

    day 3 part 2

--- 2022/day03/run.py
+++ 2022/day03/run.py
@@ -7,10 +7,19 @@ def main():
         input_lines = list(map(str.strip, in_f.readlines()))
 
     in_both_comps = list()
-    for rucksack in map(str.strip, input_lines):
+    group_badges = list()
+    group_rucksacks = list()
+    for n, rucksack in enumerate(input_lines):
         l = int(len(rucksack) / 2)
         comp1, comp2 = rucksack[l:], rucksack[:l]
         in_both_comps.extend(list({i for i in rucksack if i in comp2 and i in comp1}))
+
+        group_rucksacks.append(rucksack)
+        if len(group_rucksacks) % 3 == 0:
+            group_badges.append(
+                [i for i in group_rucksacks[0] if i in group_rucksacks[1] and i in group_rucksacks[2]][0]
+            )
+            group_rucksacks = list()
 
     def get_prio_for_item_reducer(r, i):
         if i.islower():
@@ -19,9 +28,10 @@ def main():
             return r + ord(i) - ord("A") + 27
 
     sum_prios = reduce(get_prio_for_item_reducer, in_both_comps, 0)
+    sum_prios_badges = reduce(get_prio_for_item_reducer, group_badges, 0)
 
     print("Answer part 1:", sum_prios)
-    print("Answer part 2:")
+    print("Answer part 2:", sum_prios_badges)
 
 
 if __name__ == "__main__":
