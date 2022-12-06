93605bdb1321aba3e096492b3b35fc7cea56509f
Wolfgang Maurer <wma@aneo.at>
2022-12-06 10:10:57 +0100

    day 06 part1

---
+++ 2022/day06/run.py
@@ -0,0 +1,22 @@
+def main():
+    input_str: str = "nppdvjthqldpwncqszvftbrmjlhg"
+    with open("input.txt", "r") as in_f:
+        input_str = in_f.read()
+
+    MARKER_LEN = 4
+    marker = input_str[:MARKER_LEN]
+    marker_idxs = list()
+    for i, c in enumerate(input_str[MARKER_LEN:]):
+        marker = marker[1:] + c
+        if any(marker.count(cm) > 1 for cm in marker[1:]):
+            continue
+        else:
+            marker_idxs.append(i + MARKER_LEN + 1)
+            break
+
+    print("Answer part 1:", marker_idxs[0])
+    print("Answer part 2:")
+
+
+if __name__ == "__main__":
+    main()
