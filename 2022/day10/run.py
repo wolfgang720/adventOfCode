69617144c3d41443bdddd62cd5dd2568fa9bd9c0
Wolfgang Maurer <wma@aneo.at>
2022-12-10 13:17:25 +0100

    day 10 part 2

--- 2022/day10/run.py
+++ 2022/day10/run.py
@@ -8,6 +8,10 @@ def main():
 
     signal_strength = 0
 
+    BUFFER_LEN = 40 * 6
+    crt_pos = 0
+    crt_buffer = "." * BUFFER_LEN
+
     pc = -1
     cycles = 1
     instr_cycles_left = 0
@@ -28,13 +32,19 @@ def main():
                 instr_cycles_left = 2
 
         if cycles in (20, 60, 100, 140, 180, 220):
-            print(cycles * regx, cycles, regx)
+            # print(cycles * regx, cycles, regx)
             signal_strength = signal_strength + (cycles * regx)
+
+        # pixel visible?
+        crt_pos = cycles % BUFFER_LEN - 1
+        print(crt_pos, regx)
+        crt_buffer = crt_buffer[:crt_pos] + ("#" if abs(regx - crt_pos % 40) <= 1 else ".") + crt_buffer[crt_pos + 1 :]
 
         instr_cycles_left -= 1
         cycles += 1
 
-    print("Answer:", signal_strength)
+    print("Answer 1:", signal_strength)
+    print("Answer 2:", "\n" + "\n".join(crt_buffer[x * 40 : (x + 1) * 40] for x in range(6)))
 
 
 if __name__ == "__main__":
