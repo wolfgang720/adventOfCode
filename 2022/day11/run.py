d9e1e4b83d316e9a344caa49a239813a8d713edd
Wolfgang Maurer <wma@aneo.at>
2022-12-12 00:49:55 +0100

    day 11 part 2

--- 2022/day11/run.py
+++ 2022/day11/run.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from dataclasses import dataclass
 from typing import List
@@ -11,12 +12,6 @@ class Monkey:
     true_monkey: int
     false_monkey: int
     inspected: List[int]
-
-
-def compute_worry_lvl(old, fs):
-    new = 0
-    _locals = locals()
-    print(_locals)
 
 
 new = 0
@@ -38,7 +33,12 @@ def main():
         false_monkey = int(m_lines[5].split()[-1])
         monkeys.append(Monkey(items, worry_lvl_f, test_div, true_monkey, false_monkey, list()))
 
-    for _ in range(20):
+    # with help from google: find common multiple of all dividers
+    common_multiple = 1
+    for m in monkeys:
+        common_multiple *= m.test_div
+
+    for r in range(10000):
         for monkey in monkeys:
             for old in monkey.items:
                 # global new
@@ -46,8 +46,9 @@ def main():
                 monkey.inspected.append(old)
 
                 exec("global new; " + monkey.worry_lvl_f, globals(), _locals)  # new = f(old)
-                new_worry_lvl = new // 3
-                # print(old, monkey.worry_lvl_f, new, new_worry_lvl)
+
+                # common_multiple as boundary for nunmbers: we don't lose any information regarding our problem (test divisions)
+                new_worry_lvl = new % common_multiple
 
                 # test
                 if new_worry_lvl % monkey.test_div == 0:
