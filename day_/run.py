556ef1635346459fca88282103f3ab663a7756b8
Wolfgang Maurer <wma@aneo.at>
2022-12-14 16:25:05 +0100

    add day template

---
+++ day_/run.py
@@ -0,0 +1,14 @@
+from typing import List, Union
+
+
+def main():
+    input_lines = ""
+    with open("input.txt", "r") as in_f:
+        input_lines = in_f.readlines()
+    # input_lines = """"""
+
+    print("Answer:")
+
+
+if __name__ == "__main__":
+    main()
