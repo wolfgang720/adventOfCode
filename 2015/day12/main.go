a25b502d9b1435e8694b9d476033d476c85c7a02
Wolfgang Maurer <wolfgangmaurer@Wolfgangs-Mac-mini.local>
2023-12-09 23:06:03 +0100

    day 12 part 1

---
+++ 2015/day12/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func readInputFile() (string, error) {
+	b, err := os.ReadFile("./input.txt") // just pass the file name
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func parseChildren(input interface{}) int {
+	var numbersCnt int
+	switch _input := input.(type) {
+	case []interface{}:
+		fmt.Println("array")
+		for _, elem := range _input {
+			numbersCnt += parseChildren(elem)
+		}
+		return numbersCnt
+	case map[string]interface{}:
+		fmt.Println("object")
+		for _, v := range _input {
+			// numbersCnt += parseChildren(k)
+			numbersCnt += parseChildren(v)
+		}
+		return numbersCnt
+	case string:
+		fmt.Println("string")
+		numbersCnt = 0
+		return numbersCnt
+	case float64:
+		fmt.Println("int")
+		numbersCnt = int(_input)
+		return numbersCnt
+	default:
+		fmt.Println("other", reflect.TypeOf(_input))
+	}
+	return numbersCnt
+}
+
+func main() {
+	content, err := readInputFile()
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	var data interface{}
+	json.Unmarshal([]byte(content), &data)
+	result := parseChildren(data)
+	fmt.Println(result)
+}
